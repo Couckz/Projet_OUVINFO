cc11f1a2ccdfd7628220ffd65eaec1805c95742b
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-03-13 14:51:28 +0100

    projet

--- game_file/gamestate.py
+++ game_file/gamestate.py
@@ -1,6 +1,10 @@
 """ 
 Ce fichier définit l'état actuel du jeu"""
+from gameconfig import Gameconfig
 
 class Gamestate: 
     def __init__(self):
         pass
+    
+    def draw(self, window):
+        window.blit(Gameconfig.BACKGROUND_LEVEL1, (0,0))
--- game_file/main.py
+++ game_file/main.py
@@ -3,13 +3,16 @@ fichier principale du jeu """
 
 import pygame
 from gameconfig import Gameconfig
+from gamestate import Gamestate
 
-def gameloop():
+def gameloop(window):
     quitting = False
+    game_state = Gamestate()
     while not quitting : 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitting = True
+            game_state.draw(window)
             pygame.display.update()
 
     
@@ -18,5 +21,6 @@ if __name__ == "__main__":
     window = pygame.display.set_mode((Gameconfig.LONGUEUR_LEVEL1, Gameconfig.LARGEUR_LEVEL1))
     pygame.display.set_caption("Avoid Bats")
     Gameconfig.init()
+    gameloop(window)
     pygame.quit()
     quit()
