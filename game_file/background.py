433db68f2f123262d90685bd2fea678799ac3c65
eportelli26 <emmalyne.portelli@gmail.com>
2026-03-23 18:35:23 +0100

    Resolution conflit

--- game_file/background.py
+++ game_file/background.py
@@ -1,39 +1,12 @@
 """ 
 contient la classe qui gère le joueur"""
 import pygame
+import math
 from gameconfig import Gameconfig
 
-class Background(pygame.sprite.Sprite):
-    LEFT = -1
-    RIGHT = 1
-    NONE = 0
-    
-    def __init__(self, x):
-        pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(x, Gameconfig.Y_PLATEFORM, Gameconfig.PLAYER_H, Gameconfig.PLAYER_W)
-        self.sprite_count = 0
-        self.direction = Player.NONE
-        self.image = Player.image[self.direction][self.sprite_count//Gameconfig.NB_FRAMES_PER_SPRITE_PLAYER]
-        self.mask = Player.mask[self.direction][self.sprite_count//Gameconfig.NB_FRAMES_PER_SPRITE_PLAYER]
-        self.vx = 0
-        self.vy = 0
-        
-    def draw(self, window):
-        window.blit(self.image, self.rect.topleft)
-    
-    def advance_state(self, next_move):
-        fx = 0
-        fy = 0
-        if next_move.left:
-            fx = Gameconfig.FORCE_LEFT
-            BACKGROUND_LEVEL1.direction = Player.LEFT
-        elif next_move.right:
-            fx = Gameconfig.FORCE_RIGHT    
-            BACKGROUND_LEVEL1.direction = Player.RIGHT 
-            
-        x,y = self.rect.topleft
-        vx_min = -x/Gameconfig.DT
-        vx_max = (Gameconfig.WINDOW_W-Gameconfig.PLAYER_W-x)/Gameconfig.DT
-        self.vx = fx*Gameconfig.DT
-        self.vx = min(self.vx, vx_max)
-        self.vx = max(self.vx, vx_min)
+class BG (pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load("../img_file/level1.png").convert()
+
+    def draw(self,window,seuil) :
+        window.blit(self.image,(seuil,0))
--- game_file/gamestate.py
+++ game_file/gamestate.py
@@ -3,15 +3,24 @@ Ce fichier définit l'état actuel du jeu"""
 from gameconfig import Gameconfig
 from player import *
 from move import Move
+from background import BG
 
 class Gamestate: 
+    
+
     def __init__(self):
         self.player = Player(12)
+        self.seuil = 0 
+        self.bg = BG()
     
+     
     def draw(self, window):
-        window.blit(Gameconfig.BACKGROUND_LEVEL1, (0,0))
+        # window.blit(Gameconfig.BACKGROUND_LEVEL1, (0,0))
+        self.bg.draw(window,self.seuil)
         self.player.draw(window)
-        
+
+     
     def advance_state(self, next_move):
         self.player.advance_state(next_move)
+        self.seuil = max(self.seuil+Gameconfig.D_SEUIL, Gameconfig.seuil_max)
     
