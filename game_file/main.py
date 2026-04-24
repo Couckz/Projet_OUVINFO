dd0e825f88a7d7e02d653dd11c7af2f3cb3fec2b
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-04-24 18:05:51 +0200

    plein ecran

--- game_file/main.py
+++ game_file/main.py
@@ -42,7 +42,8 @@ def gameloop(window):
     
 if __name__ == "__main__":
     pygame.init()
-    window = pygame.display.set_mode((Gameconfig.LONGUEUR_LEVEL1, Gameconfig.LARGEUR_LEVEL1))
+    info = pygame.display.Info()
+    window = pygame.display.set_mode((Gameconfig.LONGUEUR_LEVEL1, Gameconfig.LARGEUR_LEVEL1)) #pygame.SCALED | pygame.FULLSCREEN
     pygame.display.set_caption("monjeu")
     Gameconfig.init()
     Player.init_sprites()
